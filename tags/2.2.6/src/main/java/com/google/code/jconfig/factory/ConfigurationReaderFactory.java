d40ed71da24575bab3ea8a20780bb399816ab9dd
gabriele.fedeli@gmail.com <gabriele.fedeli@gmail.com@63be3a57-d8d0-5c26-7126-a450ea128533>
2011-09-15 09:42:42 +0000

    released 2.2.6 version

---
+++ tags/2.2.6/src/main/java/com/google/code/jconfig/factory/ConfigurationReaderFactory.java
@@ -0,0 +1,47 @@
+/*
+ * @(#)ConfigurationReaderFactory.java          Data: 12/set/2011
+ *
+ *
+ * Copyright 2011 Gabriele Fedeli
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.google.code.jconfig.factory;
+
+import com.google.code.jconfig.exception.ConfigurationParsingException;
+import com.google.code.jconfig.reader.ConfigurationReader;
+import com.google.code.jconfig.reader.IConfigurationReader;
+
+/**
+ * <p>
+ *    Factory for building a configuration reader
+ * </p>
+ *
+ * @author Gabriele Fedeli (gabriele.fedeli@gmail.com)
+ */
+public abstract class ConfigurationReaderFactory {
+
+	/**
+	 * <p>
+	 *    Return an instance of {@link ConfigurationReader} class.
+	 * </p>
+	 * 
+	 * @return an instance of {@link ConfigurationReader} class
+	 * @throws ConfigurationParsingException
+	 */
+	public static IConfigurationReader getReader() throws ConfigurationParsingException {
+		return new ConfigurationReader();
+	}
+}
